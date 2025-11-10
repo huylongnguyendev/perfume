cf88f7bfc6b23a591105a420517258eb57599364
Nguyen Huy Long <nguyenhuylong.dev@gmail.com>
2025-11-10 18:19:43 +0700

    commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -38,4 +38,6 @@ connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`)
   })
+}).catch((err) => {
+  console.error("MongoDB connection error:", err);
 })
