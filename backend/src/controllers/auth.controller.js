7798a19a8574f824f15a451a7f65474e34a7db3f
Nguyen Huy Long <nguyenhuylong.dev@gmail.com>
2025-11-10 21:49:46 +0700

    commit

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -134,7 +134,7 @@ export const authRefreshToken = async (req, res) => {
 
         res.cookie("refreshToken", refreshToken, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
+            secure: true,
             sameSite: "None",
             maxAge: REFRESH_TOKEN_TTl
         })
