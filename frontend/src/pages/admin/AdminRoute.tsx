4090c7635093faae2da0a1f79b4d6420d00f5925
Nguyen Huy Long <nguyenhuylong.dev@gmail.com>
2025-11-11 10:34:54 +0700

    commit

--- frontend/src/pages/admin/AdminRoute.tsx
+++ frontend/src/pages/admin/AdminRoute.tsx
@@ -8,7 +8,7 @@ const AdminRoute = () => {
   const { user, loading } = useSelector((state: RootState) => state.auth)
 
   if (loading === "loading") return <Loading />
-  if (!user?.admin && loading === "success") return <Navigate to="/shop" replace />
+  if (!user?.admin && loading === "success" || !user) return <Navigate to="/shop" replace />
 
   return <Outlet />
 }
