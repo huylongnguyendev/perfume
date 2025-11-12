acd3f6ad15cfc0f00d9e230d947e67845397ba2d
Nguyen Huy Long <nguyenhuylong.dev@gmail.com>
2025-11-12 17:01:32 +0700

    commit

--- frontend/src/components/user/filter/SearchFilter.tsx
+++ frontend/src/components/user/filter/SearchFilter.tsx
@@ -3,7 +3,9 @@ import { Input } from '@/components/ui/input'
 import { fetchAllProduct } from '@/redux/productListSlice'
 import type { AppDispatch, RootState } from '@/redux/store'
 import { Search } from 'lucide-react'
+import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { debounce } from 'lodash'
 
 interface Props {
   filters: {
@@ -17,7 +19,19 @@ interface Props {
 
 const SearchFilter = ({ filters }: Props) => {
   const search = useSelector((state: RootState) => state.productFilter.search)
-  const dispacth = useDispatch<AppDispatch>()
+  const dispatch = useDispatch<AppDispatch>()
+
+  const [localSearch, setLocalSearch] = useState(search)
+
+  const debouncedSearch = debounce((value: string) => {
+    dispatch(fetchAllProduct({ ...filters, search: value }))
+  }, 500)
+
+  useEffect(() => {
+    debouncedSearch(localSearch)
+    return () => debouncedSearch.cancel()
+  }, [localSearch])
+
 
   return (
     <>
@@ -25,7 +39,7 @@ const SearchFilter = ({ filters }: Props) => {
         <div className=" relative">
           <Input
             value={search}
-            onChange={(e) => dispacth(fetchAllProduct({ ...filters, search: e.target.value }))}
+            onChange={(e) => setLocalSearch(e.target.value)}
             placeholder="Tìm kiếm..."
             className="bg-background py-5 pe-10"
           />
