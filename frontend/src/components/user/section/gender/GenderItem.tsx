bdb6a5e37002fcdc8252b1e7e01974611c9bbdb0
Nguyen Huy Long <nguyenhuylong.dev@gmail.com>
2025-11-12 17:24:57 +0700

    commit

--- frontend/src/components/user/section/gender/GenderItem.tsx
+++ frontend/src/components/user/section/gender/GenderItem.tsx
@@ -10,7 +10,7 @@ interface Props {
 const GenderItem = ({ gender }: Props) => {
   return (
     <>
-      <div className={"bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 rounded-md p-4 space-y-2 text-white border transition-all duration-300 group-hover:scale-[1.02]"}>
+      <div className={"bg-gradient-to-br size-full from-indigo-500 via-purple-500 to-pink-500 rounded-md p-4 space-y-2 text-white border transition-all duration-300 group-hover:scale-[1.02]"}>
         <h2 className="text-white font-semibold text-lg">{gender.name}</h2>
         <p className="">{gender.desc}</p>
       </div>
